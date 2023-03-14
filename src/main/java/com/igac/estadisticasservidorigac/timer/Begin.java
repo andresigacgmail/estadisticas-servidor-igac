ef0d4a56d763424a0ea153868dc9eb9017f63cdf
homeristico <homeristico@hotmail.com>
2023-03-13 21:09:47 -0500

    Begin timer working

--- src/main/java/com/igac/estadisticasservidorigac/timer/Begin.java
+++ src/main/java/com/igac/estadisticasservidorigac/timer/Begin.java
@@ -4,34 +4,27 @@ import com.igac.estadisticasservidorigac.services.impl.EstadisticaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-@Service
+@Service @EnableScheduling
 public class Begin {
 
-    @Autowired private static EstadisticaServiceImpl estadisticaService;
+    @Autowired private EstadisticaServiceImpl estadisticaService;
 
     @EventListener
     public void eventListener(ContextRefreshedEvent contextRefreshedEvent) {
-         /*ScheduledExecutorService executorService;
-        executorService = Executors.newSingleThreadScheduledExecutor();
-        executorService.scheduleAtFixedRate(Begin::run, 0, 8, TimeUnit.SECONDS);*/
-
 
     }
 
     @Scheduled(fixedDelay = 8000)
     public  void run() {
-       System.out.println("seep");
-       try {
-           estadisticaService.guardarEstadosAutomatico();
-       }catch (Exception ex){
-           System.out.println(ex.getMessage());
-       }
+        System.out.println("seep");
+        try {
+            estadisticaService.guardarEstadosAutomatico();
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
